cffea02ab9a3d9049a93a6cdbe87fcd5c7ef2bb3
WhiteW00lf <mocha_cookie_crumble@proton.me>
2026-02-23 13:14:00 +0530

    Created raw schema

---
+++ business_questions.md
@@ -0,0 +1,7 @@
+# Business questions
+
+1. Total revenue by month?
+2. Which area had the highest revenue?
+3. Revenue by taxi company?
+4. Peak pickup hours?
+
---
+++ queries/create_schema.sql
@@ -0,0 +1,3 @@
+CREATE SCHEMA IF NOT EXISTS `deportfolio-486507.taxi_raw`;
+CREATE SCHEMA IF NOT EXISTS `deportfolio-486507.taxi_staging`;
+CREATE SCHEMA IF NOT EXISTS `deportfolio-486507.taxi_marts`;

be9b4638e36dd5aa2ccb1ee462f134ea8a0cc33d
WhiteW00lf <mocha_cookie_crumble@proton.me>
2026-02-25 15:06:47 +0530

    allowed fare to be NULL

--- marts/facts/fact_trips.sql
+++ marts/facts/fact_trips.sql
@@ -5,7 +5,7 @@ s.Trip_ID,
 CAST(FORMAT_DATE('%Y%m%d', DATE(s.Trip_Start)) AS INT64) AS pickup_date_id,
 CAST(FORMAT_DATE('%Y%m%d', DATE(s.Trip_End)) AS INT64) AS dropoff_date_id,
 dc.company_id,
-COALESCE(s.Fare,0) AS fare_amount,
+s.Fare AS fare_amount,
 COALESCE (s.Trip_Total,0) AS trip_total,
 COALESCE (s.Tips,0) AS tips,
 COALESCE(s.Tolls,0) AS tolls
